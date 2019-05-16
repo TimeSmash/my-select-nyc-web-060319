88d694d8a0b32400d1fc34513a2c16dfb2ff9a6c
mcumm64 <mcumm64@gmail.com>
2019-05-16 00:30:30 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
-def my_select(collection)
- # your code here!
+def my_select(arr)
+  if block_given?
+    i =0
+    new_arr = []
+    while i <arr.length
+      p yield(arr[i])
+      if yield(arr[i]) == true
+        new_arr << arr[i]
+      end
+      i += 1
+    end
+  end
+  new_arr
 end
+
+# [1,2,3,4].select { |num| num.even?} # => [2,4]
