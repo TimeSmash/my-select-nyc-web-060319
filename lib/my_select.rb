f08a870cb6df6fb5f85a42c34aa8491a23a8460c
mcumm64 <mcumm64@gmail.com>
2019-05-16 00:37:23 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -19,4 +19,6 @@ def my_select(arr)
   new_arr
 end
 
+#so basically we take each broken down again. Use i = 0 to start at index-0 of array, also create new_arr which will have elements that evaluate as true after being subjected to block added to it. That's the whole purpose of the if statement here--all it's saying is that IF the element subjected to the block evaluates as true (ex. element passed is 4, block is |element| > 2, this evaluates to true), add that element to new_arr (in case of said example, 4 is added. If element passed is 1 --> 1 is not greater than 2, evaluates as false, and 1 is not added to new_arr)
+
 # [1,2,3,4].select { |num| num.even?} # => [2,4]
