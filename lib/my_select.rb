ea71a94569f9c266a5a427b2a621212a7ecd525b
mcumm64 <mcumm64@gmail.com>
2019-05-16 00:32:44 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,12 @@ def my_select(arr)
     i =0
     new_arr = []
     while i <arr.length
-      p yield(arr[i])
+      # p yield(arr[i]) ==> puts the boolean value. Enable this to see how each item in the list evaluated when subjected to the block. 
+      
+      #ex. nums = [1,2,3]
+      #my_select(nums) {|num| num > 2}
+      
+      #nums[0] = 1 ==>
       if yield(arr[i]) == true
         new_arr << arr[i]
       end
